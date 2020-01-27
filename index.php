7a01a504cef7057d560230636c8d10dc9e0223f2
Evgenii Mishkin <emishkin@mail.greenriver.edu>
2020-01-27 13:34:48 -0500

    Step 1: Created index

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    //Start session
+    session_start();
+
+    // Turn on error reporting - this is critical!
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    //Require the autoload file
+    require_once('vendor/autoload.php');
+
+    //Create an instance of the base class
+    $f3 = Base::instance();
+
+    //Define a default route
+    $f3->route('GET /', function (){
+        //$view = new Template();
+        //echo $view->render('views/home.html');
+        echo "<h1>Hello, world!</h1>";
+    });
+
+    //Run fat free
+    $f3->run();
